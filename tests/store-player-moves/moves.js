104f18370d5225481256d4d508709d050fb5150e
P2586846 <mahdiharoun1993@gmail.com>
2020-10-23 12:21:34 +0100

    store players moves tests

---
+++ tests/store-player-moves/moves.js
@@ -0,0 +1,19 @@
+let playerOneArray = []
+
+let playerTwoArray = []
+
+//tracking move
+let currentPlayerIndex = 1
+
+if (currentPlayerIndex === 1) { 
+    playerOneArray.push(2)
+
+    
+
+}
+    console.log(playerOneArray)
+
+
+    
+
+
